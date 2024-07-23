4b4789e6da15038374a25ae2de6eb93f7b60a8d8
Joohyun Kim <corinthionia@gmail.com>
2024-07-23 18:12:29 +0900

    3주차 알고리즘 제출 : corinthionia (#12)
    * 알고리즘 제출 : corinthionia
    
    * 3주차 알고리즘 제출 : corinthionia

---
+++ 3week/corinthionia/1943.py
@@ -0,0 +1,10 @@
+for _ in range(3):
+    N = int(input())
+    coins = dict()
+    
+    for _ in range(N):
+        a, b = map(int, input().split())
+        coins[a] = b
+        
+    print(coins)
+    # 아직,, 미완성,, 틀려서,, 다시 푸는 중,,
