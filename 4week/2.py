5d097d4839a61d28d8c2b9f890db4b950ebf6bef
DongHyeonWon <dhe7700@naver.com>
2024-08-06 17:41:55 +0900

    5주차 알고리즘 제출 : hellol77

--- 4week/2.py
+++
@@ -1,9 +0,0 @@
-def solution(k, m, score):
-    score.sort(reverse=True)
-    answer=0
-    for i in range(len(score)//m):
-        minNumber=min(score[0+m*i:m+m*i])
-        answer+=minNumber*m
-
-
-    return answer
