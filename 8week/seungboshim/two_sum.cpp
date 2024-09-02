11f16bae585287ae378993a5218a485896117b6a
jkw <ashjkw@gmail.com>
2024-09-02 10:16:30 +0900

    9주차 알고리즘 제출 : jeonkyungwon

--- 8week/seungboshim/two_sum.cpp
+++ 8week/seungboshim/two_sum.cpp
@@ -1,36 +1,34 @@
-#include <vector>
+// #include <vector>
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int maxIdx = nums.size()-1;
-        vector<pair<int,int>> v;
-        for (int i=0; i<=maxIdx; i++) {
-            v.push_back({nums[i], i});
-        }
-        sort(v.begin(), v.end());
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int maxIdx = nums.size()-1;
+//         vector<pair<int,int>> v;
+//         for (int i=0; i<=maxIdx; i++) {
+//             v.push_back({nums[i], i});
+//         }
+//         sort(v.begin(), v.end());
 
-        int left = 0;
-        int right = maxIdx;
-        int sum = 0;
+//         int left = 0;
+//         int right = maxIdx;
+//         int sum = 0;
 
-        while (left < right) {
-            sum = v[left].first + v[right].first;
-            // 2 7 11 15
-            if (sum == target) {
-                break;
-            } else if (sum > target) {
-                right--;
-            } else {
-                left++;
-            }
-        }
+//         while (left < right) {
+//             sum = v[left].first + v[right].first;
+//             // 2 7 11 15
+//             if (sum == target) {
+//                 break;
+//             } else if (sum > target) {
+//                 right--;
+//             } else {
+//                 left++;
+//             }
+//         }
 
-
-
-        vector<int> ans;
-        ans.push_back(v[left].second);
-        ans.push_back(v[right].second);
-        return ans;
-    }
-};
+//         vector<int> ans;
+//         ans.push_back(v[left].second);
+//         ans.push_back(v[right].second);
+//         return ans;
+//     }
+// };
