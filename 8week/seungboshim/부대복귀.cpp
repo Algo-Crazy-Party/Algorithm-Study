998879d9218aeeaf6e0367400330d38bcf574040
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2024-09-10 18:39:42 +0900

    9주차 알고리즘 제출 : Jeonkyungwon (#37)
    * 알고리즘 제출 : jeonkyungwon
    
    * 알고리즘 제출 : jeonkyungwon
    
    * 2주차 알고리즘 제출 : jeonkyungwon
    
    * 3주차 알고리즘 제출 : jeonkyungwon
    
    * 5주차 알고리즘 제출 : jeonkyungwon
    
    * 6주차 알고리즘 제출 : jeonkyungwon
    
    * 7주차 알고리즘 제출 : Jeonkyungwon
    
    * 8주차 알고리즘 제출 : jeonkyungwon
    
    * 9주차 알고리즘 제출 : jeonkyungwon
    
    ---------
    
    Co-authored-by: jkw <ashjkw@gmail.com>

--- 8week/seungboshim/부대복귀.cpp
+++ 8week/seungboshim/부대복귀.cpp
@@ -1,56 +1,56 @@
-#include <string>
-#include <vector>
-#include <queue>
+// #include <string>
+// #include <vector>
+// #include <queue>
 
-using namespace std;
+// using namespace std;
 
-vector<vector<int>> makeGraph(int n, vector<vector<int>> roads) {
-    vector<vector<int>> graph(n);
-    for(auto& road: roads) {
-        int u = road[0];
-        int v = road[1];
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    
-    return graph;
-}
+// vector<vector<int>> makeGraph(int n, vector<vector<int>> roads) {
+//     vector<vector<int>> graph(n);
+//     for(auto& road: roads) {
+//         int u = road[0];
+//         int v = road[1];
+//         graph[u].push_back(v);
+//         graph[v].push_back(u);
+//     }
 
-int BFS(int start, int dest, vector<vector<int>> graph) {
-    vector<int> dist(graph.size(), -1);
-    queue<int> q;
-    
-    q.push(start);
-    dist[start] = 0;
-    
-    while (!q.empty()) {
-        int current = q.front();
-        q.pop();
-        
-        for (int n: graph[current]) {
-            if (dist[n] == -1) {
-                q.push(n);
-                dist[n] = dist[current] + 1;
-                
-                if (n == dest) {
-                    return dist[n];
-                }
-            }
-        }
-    }
-    
-    return -1;
-}
+//     return graph;
+// }
 
-vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
-    vector<int> answer;
-    vector<vector<int>> graph = makeGraph(n, roads);
-    
-    for(int s: sources) {
-        // s -> dest 그래프 최단거리
-        int result = BFS(s, destination, graph);
-        answer.push_back(result);
-    }
-    
-    return answer;
-}
+// int BFS(int start, int dest, vector<vector<int>> graph) {
+//     vector<int> dist(graph.size(), -1);
+//     queue<int> q;
+
+//     q.push(start);
+//     dist[start] = 0;
+
+//     while (!q.empty()) {
+//         int current = q.front();
+//         q.pop();
+
+//         for (int n: graph[current]) {
+//             if (dist[n] == -1) {
+//                 q.push(n);
+//                 dist[n] = dist[current] + 1;
+
+//                 if (n == dest) {
+//                     return dist[n];
+//                 }
+//             }
+//         }
+//     }
+
+//     return -1;
+// }
+
+// vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
+//     vector<int> answer;
+//     vector<vector<int>> graph = makeGraph(n, roads);
+
+//     for(int s: sources) {
+//         // s -> dest 그래프 최단거리
+//         int result = BFS(s, destination, graph);
+//         answer.push_back(result);
+//     }
+
+//     return answer;
+// }
